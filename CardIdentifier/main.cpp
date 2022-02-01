4cae2b047ef4759f23d67f92c5960b8356e695e6
JoneOA <owainjones14@hotmail.co.uk>
2022-02-01 08:50:23 +0000

    Closer to isolating the numbers and symbols from the card

--- CardIdentifier/main.cpp
+++ CardIdentifier/main.cpp
@@ -8,6 +8,22 @@
 
 using namespace std;
 
+void CallBackFunc(int event, int x, int y, int flags, void* userdata)
+{
+	if (event == cv::EVENT_LBUTTONDOWN)
+	{
+		cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	}
+	else if (event == cv::EVENT_RBUTTONDOWN)
+	{
+		cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	}
+	else if (event == cv::EVENT_MBUTTONDOWN)
+	{
+		cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+	}
+}
+
 int main()
 {
 	std::vector<cv::Point2f> cardCorners, warpedCorners(4), midPoints(4);
@@ -21,34 +37,19 @@ int main()
 	int threshL = 245;
 
 	cv::namedWindow("Sliders", cv::WINDOW_AUTOSIZE);
-	cv::namedWindow("Warped", cv::WINDOW_AUTOSIZE);
-	cv::namedWindow("Result", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("w", cv::WINDOW_AUTOSIZE);
 	cv::createTrackbar("ThreshL", "Sliders", &threshL, 255);
 
-	cv::Mat img, cardImg, M, warpedImg, warpedImg2, result;
+	cv::Mat cimg, img, cardImg, M, warpedImg, warpedImg2, result, sub;
 
 	set<filesystem::path> sortedCards;
-
-
-	//for (auto& file : filesystem::directory_iterator("C:\\Users\\Owain\\source\\repos\\CardIdentifier\\CardIdentifier\\Resources\\Suit")) {
-	//	img = cv::imread(file.path().string());
-
-	//	cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
-
-	//	cv::GaussianBlur(img, img, cv::Size(3, 3), 0);
-
-	//	cv::threshold(img, img, 123, 255, cv::THRESH_BINARY_INV);
-
-	//	cv::imshow("Suit", img);
-
-	//	cv::waitKey(0);
-
-	//}
-
+	cv::setMouseCallback("w", CallBackFunc, NULL);
 	while (true) {
+		std::string cardNumber;
+		int nonZeroCount = 10000000;
 		cap >> cardImg;
 
-		cardImg.copyTo(img);
+		cardImg.copyTo(cimg);
 		cv::cvtColor(cardImg, cardImg, cv::COLOR_BGR2GRAY);
 		cv::threshold(cardImg, cardImg, threshL, 255, cv::THRESH_BINARY_INV);
 
@@ -56,16 +57,14 @@ int main()
 
 		for (std::vector<cv::Point> edge : edges) {
 			cv::approxPolyDP(edge, shapes, 0.01*cv::arcLength(edge, true), true);
-
-			
-			if (shapes.size() == 4) {
+			if (shapes.size() == 4) { 
 				rectangles.push_back(shapes);
 			}
 		}
 		
 		for (int i = 0; i < rectangles.size(); i++) {
 			if (cv::contourArea(rectangles[i]) > 200) {
-				cv::drawContours(img, rectangles, i, cv::Scalar(255, 0, 255));
+				cv::drawContours(cimg, rectangles, i, cv::Scalar(255, 0, 255));
 
 				for (int j = 0; j < rectangles[i].size(); j++) {
 					cardCorners.push_back(cv::Point2f((float)rectangles[i][j].x, (float)rectangles[i][j].y));
@@ -91,38 +90,70 @@ int main()
 
 				cv::warpPerspective(cardImg, warpedImg, M, warpedImgSize); 
 
-				cv::resize(warpedImg, warpedImg, cv::Size(620, 870));
-				cv::GaussianBlur(warpedImg, warpedImg, cv::Size(13, 13), 0);
-				cv::findContours(warpedImg, warpedCont, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
-				for (int l = 0; l < warpedCont.size(); l++) {
-					cv::rectangle(warpedImg, boundingRect(warpedCont[l]), cv::Scalar::all(255));
+				if (warpedImgSize.width > warpedImgSize.height) {
+					cv::rotate(warpedImg, warpedImg, cv::ROTATE_90_CLOCKWISE);
 				}
-				warpedImg2 = warpedImg;
-				//cv::cvtColor(warpedImg, warpedImg, cv::COLOR_BGR2GRAY);
-				//cv::threshold(warpedImg, warpedImg, 245, 255, cv::THRESH_BINARY_INV);
+				
+
+				cv::Size s(20, 70);
+				cv::Point p(10, 35);
+				cv::getRectSubPix(warpedImg, s, p, warpedImg);
+
+				//cv::threshold(warpedImg, warpedImg, 123, 255, cv::THRESH_BINARY);
+
+				cv::resize(warpedImg, warpedImg, cv::Size(70, 120));
+				cv::findContours(warpedImg, warpedCont, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+
+				for (int l = 0; l < warpedCont.size(); l++) {
+					if (boundingRect(warpedCont[l]).area() > 1000) {
+						cv::rectangle(warpedImg, boundingRect(warpedCont[l]), cv::Scalar::all(255));
+						std::cout << l << ": " << boundingRect(warpedCont[l]).area() << "; ";
+					}
+				}
+				std::cout << "\n";
 
 				for (auto& file : filesystem::directory_iterator("C:\\Users\\Owain\\source\\repos\\CardIdentifier\\CardIdentifier\\Resources\\Numbers")) {
 					img = cv::imread(file.path().string());
-					cv::resize(img, img, cv::Size(70, 120));
 					cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
 					cv::GaussianBlur(img, img, cv::Size(3, 3), 0);
 					cv::threshold(img, img, 123, 255, cv::THRESH_BINARY_INV);
 
 					cv::findContours(img, numberCont, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+					cv::Size S;
+					cv::Point P;
 					for (int k = 0; k < numberCont.size(); k++) {
-						cv::rectangle(img, boundingRect(numberCont[k]), cv::Scalar::all(255));
+						cv::Rect numR = boundingRect(numberCont[k]);
+						if (numR.area() > 20000) {
+							cv::rectangle(img, numR, cv::Scalar::all(255));
+							S = numR.size();
+							P = cv::Point(numR.x + numR.width / 2, numR.y + numR.height / 2);
+						}
 					}
+					//cv::getRectSubPix(img, S, P, img);
+					//cv::resize(img, img, cv::Size(70, 120));
+
+					//cv::subtract(warpedImg, img, sub);
+
+					/*if (cv::countNonZero(sub) < nonZeroCount) {
+						cardNumber = file.path().string();
+						nonZeroCount = cv::countNonZero(sub);
+						std::cout << cardNumber << ": " << nonZeroCount << "\n";
+					}*/
+
 					cv::imshow("mg", img);
 					cv::imshow("w", warpedImg);
+					//cv::imshow("sub", sub);
 					cv::waitKey(0);
 				}
 
 				cardCorners.clear();
 			}
 		}
-
+		cv::imshow("E", cimg);
 		cv::imshow("D", cardImg);
 		rectangles.clear();
+
+		std::cout << cardNumber << "\n";
 		cv::waitKey(0);
 	}
 
